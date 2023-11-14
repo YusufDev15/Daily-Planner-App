95e2f6b7bf447e57231f3189d617088312be72e1
Yusuf <145934201+YusufDev15@users.noreply.github.com>
2023-11-14 16:39:41 +0000

    change the font awesome link, change the timeblovk element inside the if statement to 'this'

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //browser runs the HTML and css first
 $(document).ready(function () {
   var currentDay = $("#currentDay");
-
+  var timeBlocks = $(".time-block");
   // use dayjs to  display the date
   var currentDate = dayjs().format("dddd, MMMM DD, YYYY");
   currentDay.text(currentDate);
@@ -9,19 +9,19 @@ $(document).ready(function () {
   function everyHour() {
     //find the current hour
     var currentHour = dayjs().hour();
-    var timeBlocks = $(".time-block");
+
     //loop over each hour
     timeBlocks.each(function () {
       //retrieve the value of the 'id' attribute of the timeblock element using jquery attr
       //split the value of the 'id' attribute into an array of substrings
-      var eachHour = parseInt(timeBlocks.attr("id").split("-")[1]); //retrieve the second element after splitting
+      var eachHour = parseInt($(this).attr("id").split("-")[1]); //retrieve the second element after splitting
       //compare eachHour and currentHour and assign the right class
       if (eachHour < currentHour) {
-        timeBlocks.find(".description").addClass("past"); //maybe $(this)
+        $(this).find(".description").addClass("past");
       } else if (eachHour === currentHour) {
-        timeBlocks.find(".description").addClass("present");
+        $(this).find(".description").addClass("present");
       } else {
-        timeBlocks.find(".description").addClass("future");
+        $(this).find(".description").addClass("future");
       }
     });
   }
